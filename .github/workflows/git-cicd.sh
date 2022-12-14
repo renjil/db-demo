d8c1ebbaccd4a9a6c8a8c2b05ebdf05bc2bfac08
Renji <renjil@users.noreply.github.com>
2022-12-15 00:17:09 +1100

    Update git-cicd.sh

--- .github/workflows/git-cicd.sh
+++ .github/workflows/git-cicd.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
 repo_id=$(databricks repos list --path-prefix /Repos/renji.harold@databricks.com/db-demo | jq '.repos | .[].id')
+echo "Repo ID is $repo_id"
+
 databricks repos update --repo-id $repo_id --branch staging
 echo "successfully updated staging branch on databricks"
+
 # update databricks job to point to staging branch for executing integration test
