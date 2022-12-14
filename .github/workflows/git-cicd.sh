f0a99bbc8d2abcc39f4e88659c4d74a163040847
Renji <renjil@users.noreply.github.com>
2022-12-15 01:09:28 +1100

    Update git-cicd.sh

--- .github/workflows/git-cicd.sh
+++ .github/workflows/git-cicd.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo "host: $DATABRICKS_HOST"
-
-# repo_id=$(databricks repos list --path-prefix /Repos/renji.harold@databricks.com/db-demo | jq '.repos | .[].id')
+repo_id=$(databricks repos list --path-prefix /Repos/renji.harold@databricks.com/db-demo | jq '.repos | .[].id')
 #repo_id=4195692383084558
-#echo "Repo ID is $repo_id"
+echo "Repo ID is $repo_id"
 
 #databricks repos update --repo-id $repo_id --branch staging
 #echo "successfully updated staging branch on databricks"
